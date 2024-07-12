f9669b8aa55750eef8b914a561dec9df664af08e
Alexkuark <47670905+Alexkuark@users.noreply.github.com>
2024-07-12 14:43:18 +0200

    Update DeclarationApp.java

--- approche_objet/src/fr/declaration/variable/DeclarationApp.java
+++ approche_objet/src/fr/declaration/variable/DeclarationApp.java
@@ -3,8 +3,20 @@ package fr.declaration.variable;
 public class DeclarationApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		byte myByte = 5;
+		short myShort = 5;
+		int myInt = 5;
+		long myLong = 5;
+		float myFloat = 5.0f;
+		double myDouble = 5.0;
+		char myChar = '5';
+		boolean myBoulean = true;
+		String myString = "cinq";
+		
+		System.out.println(myBoulean);
+		
+		String randomString = "Voici le résultat d’un calcul :\n1+5=6";
+		System.out.println(randomString);
 	}
 
 }
