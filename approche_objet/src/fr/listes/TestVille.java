14bc83ad729cfd08ac4903987841a178d8314aa9
Alexkuark <47670905+Alexkuark@users.noreply.github.com>
2024-07-16 16:07:51 +0200

    TP15

---
+++ approche_objet/src/fr/listes/TestVille.java
@@ -0,0 +1,21 @@
+package fr.listes;
+
+import fr.listes.Ville;
+
+public class TestVille {
+
+	public static void main(String[] args) {
+
+		Ville ville1 = new Ville("Carcassonne", 47800);
+		Ville ville2 = new Ville("Carcassonne", 47800);
+		Ville ville3 = new Ville("Carcassonne", 47800);
+		
+		System.out.println(ville1.equals(ville2));
+		ville2.setNom("Nice");
+		System.out.println(ville1.equals(ville2));
+		ville3.setNbHabitants(343000);
+		System.out.println(ville1.equals(ville3));
+
+	}
+
+}
---
+++ approche_objet/src/fr/listes/Ville.java
@@ -0,0 +1,41 @@
+package fr.listes;
+
+public class Ville {
+	
+	protected String nom;
+	protected int nbHabitants;
+	
+	public Ville() {}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public int getNbHabitants() {
+		return this.nbHabitants;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public void setNbHabitants(int population) {
+		this.nbHabitants = population;
+	}
+	
+	public Ville(String nom, int population) {
+		this.setNom(nom);
+		this.setNbHabitants(population);
+	}
+	
+	public boolean equals(Object another) {
+		if(another instanceof Ville) {
+			Ville otherVille = (Ville) another;
+			return otherVille.getNom() == this.getNom() && otherVille.getNbHabitants() == this.getNbHabitants();
+		}
+		else {
+			return false;
+		}
+	}
+
+}
