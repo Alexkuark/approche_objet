05162f27267552e5e9afcd78f7ca4aa5c3c12556
Alexkuark <47670905+Alexkuark@users.noreply.github.com>
2024-07-16 10:48:44 +0200

    TP10

--- approche_objet/src/fr/diginamic/banque/TestBanque.java
+++ approche_objet/src/fr/diginamic/banque/TestBanque.java
@@ -1,12 +1,16 @@
 package fr.diginamic.banque;
 
 import fr.diginamic.banque.entites.Compte;
+import fr.diginamic.banque.entites.LivretA;
 
 public class TestBanque {
 
 	public static void main(String[] args) {
 		Compte compte = new Compte("ABC1", 30.00);
 		System.out.println(compte);
+		
+		LivretA livret = new LivretA("ABC2", 30.00, 15.0);
+		System.out.println(livret);
 	}
 
 }
---
+++ approche_objet/src/fr/diginamic/banque/entites/LivretA.java
@@ -0,0 +1,40 @@
+package fr.diginamic.banque.entites;
+
+public class LivretA extends Compte {
+	
+	protected double taux;
+	
+	public String getNumCompte() {
+		return super.getNumCompte();
+	}
+	
+	public double getSolde() {
+		return super.getSolde();
+	}
+	
+	public double getTaux() {
+		return this.taux;
+	}
+	
+	public void setSolde(double montant) {
+		super.setSolde(montant);;
+	}
+	
+	public void setTaux(double taux) {
+		this.taux = taux;
+	}
+	
+	public LivretA() {
+		super();
+	}
+	
+	public LivretA(String numero, double montant, double taux) {
+		super(numero, montant);
+		this.setTaux(taux);
+	}
+	
+	public String toString() {
+		return super.toString() + "\nTaux : " + this.getTaux();
+	}
+
+}
