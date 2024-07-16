fa978e24ca175a3d75d673e8cf7e20aad4662c19
Alexkuark <47670905+Alexkuark@users.noreply.github.com>
2024-07-16 14:49:01 +0200

    Update TestStringBuilder.java

--- approche_objet/src/fr/diginamic/chaines/TestStringBuilder.java
+++ approche_objet/src/fr/diginamic/chaines/TestStringBuilder.java
@@ -20,6 +20,17 @@ public class TestStringBuilder {
 		debut = System.currentTimeMillis();
 
 		for(int i=1; i<=100000; i++) {
+			chaine2.concat(""+i);
+		}
+		
+		fin = System.currentTimeMillis();
+		System.out.println("Temps écoulé en millisecondes :" + (fin - debut));
+		
+		chaine2 = "";
+		
+		debut = System.currentTimeMillis();
+
+		for(int i=1; i<=100000; i++) {
 			chaine2 += i;
 		}
 		
